a49ecc1ce2f15282d6f397f710d3b130f1fac939
Tobias Thorsen <tobiasthorsen@gmail.com>
2021-12-19 01:05:07 +0100

    mixed stuff

--- heat.py
+++ heat.py
@@ -1,4 +1,8 @@
 from tkinter import *
+from datetime import date
+import time
+
+
 
 window = Tk()
 
@@ -6,14 +10,19 @@ window.title("Welcome to LikeGeeks app")
 
 window.geometry('350x200')
 
-
 lbl = Label(window, text="Hello", font=("Arial Bold", 50))
 
 lbl.grid(column=0, row=0)
 
-def clicked():
 
-    lbl.configure(text="Button was clicked !!")
+def current_milli_time():
+    return round(time.time() * 1000)
+
+def clicked():
+	#today = date.ctime()
+	ms = time.time()*1000.0
+	#print("Today's date:", today)
+	lbl.configure(text="Button was clicked !!" +str( ms))
 
 btn = Button(window, text="Click Me", command=clicked)
 
@@ -24,4 +33,14 @@ chk = Checkbutton(window, text='Choose', var=chk_state)
 
 chk.grid(column=0, row=1)
 
+def mainloop():
+    print("Hello, World")
+    ms = time.time()*1000.0
+    lbl.configure(text="aa " +str( int(ms)))
+
+
+
+tkinter.after(1000,mainloop)
 window.mainloop()
+
+
