ef0664194aca53b48ddad2d90aafb6228c47a1b5
Tobias Thorsen <tobiasthorsen@gmail.com>
2024-09-16 00:02:37 +0200

    save the config

--- heat.py
+++ heat.py
@@ -74,7 +74,7 @@ class Oven:
 		self.kwh = 0.0
 		self.trackstarttime = time.time()
 		#self.calibrate1000 = 1000
-		self.temperatureCalibrate = 1.03
+		self.temperatureCalibrate = temperatureCalibrate
 
 
 	def update(self):
