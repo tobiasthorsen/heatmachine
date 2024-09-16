b870c4c932efac7393d187ddf02429f17899bc76
Tobias Thorsen <tobiasthorsen@gmail.com>
2024-09-16 22:33:22 +0200

    fix int probs

--- heat.py
+++ heat.py
@@ -90,6 +90,7 @@ class Oven:
 			gottemperature = 0
 			try:
 				tc = self.thermocouple.get()
+				print ("Raw temp " + tc)
 				calibfactor =   (tc - 100) / 700
 				calibrate = self.temperatureCalibrate100 + (self.temperatureCalibrate800 - self.temperatureCalibrate100) * calibfactor
 				tc = tc * calibrate
